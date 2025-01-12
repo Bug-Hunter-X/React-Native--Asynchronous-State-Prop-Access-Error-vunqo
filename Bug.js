94c7394eed822973b0ca9074f197a81d601769ce
BugHunter-X <askzenai@gmail.com>
2025-01-12 06:14:15 +0100

    Add Bug.js file

---
+++ Bug.js
@@ -0,0 +1,1 @@
+This error occurs when you try to access a state variable or a prop before it has been properly set. This often happens in asynchronous operations, such as API calls or timers, where the state update might not be immediate.
